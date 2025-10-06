72f7b8531e069981d6788a5d9338c7a9b56aac0b
Rakhimov Navruz <alina@Rakhimovs-MacBook-Pro.local>
2025-10-07 00:22:13 +0900

    feat: integration of like related complex lookup query

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,61 +1,115 @@
-import { ObjectId } from 'bson';
+import { ObjectId } from "bson";
 
-export const availableAgentSorts = ["createdAt","updatedAt","memberLikes","memberView","memberRank"]
+export const availableAgentSorts = [
+  "createdAt",
+  "updatedAt",
+  "memberLikes",
+  "memberView",
+  "memberRank",
+];
 
-export const availableMemberSorts = ["createdAt", "updatedAt", "memberLikes", "memberView",]
+export const availableMemberSorts = [
+  "createdAt",
+  "updatedAt",
+  "memberLikes",
+  "memberView",
+];
 
-export const availableOptions = ['propertyBarter', 'propertyRent']
+export const availableOptions = ["propertyBarter", "propertyRent"];
 export const availablePropertySorts = [
-	'createdAt',
-	'updatedAt',
-	'propertyLikes',
-	'propertViews',
-	'propertyRank',
-	'propertyPrice'
-] 
+  "createdAt",
+  "updatedAt",
+  "propertyLikes",
+  "propertyViews",
+  "propertyRank",
+  "propertyPrice",
+];
 
-
-export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews']
-export const availableCommentSorts = ['createdAt', 'updatedAt']
+export const availableBoardArticleSorts = [
+  "createdAt",
+  "updatedAt",
+  "articleLikes",
+  "articleViews",
+];
+export const availableCommentSorts = ["createdAt", "updatedAt"];
 
 /* IMAGE CONFIGURATION (config.js) */
-import { v4 as uuidv4 } from 'uuid';
-import * as path from 'path';
-import { from } from 'rxjs';
+import { v4 as uuidv4 } from "uuid";
+import * as path from "path";
+import { from } from "rxjs";
+import { T } from "./types/common";
+import { pipeline } from "stream";
 
-export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+export const validMimeTypes = ["image/png", "image/jpg", "image/jpeg"];
 export const getSerialForImage = (filename: string) => {
-	const ext = path.parse(filename).ext;
-	return uuidv4() + ext;
+  const ext = path.parse(filename).ext;
+  return uuidv4() + ext;
 };
 
 export const shapeIntoMongoObjectId = (target: any) => {
-	return typeof target === 'string' ? new ObjectId(target) : target;
+  return typeof target === "string" ? new ObjectId(target) : target;
+};
+
+export const lookupAuthMemberLiked = (
+  memberId: T,
+  targetRefId: string = "$_id"
+) => {
+  return {
+    $lookup: {
+      from: "likes",
+      let: {
+        localLikeRefId: targetRefId,
+        localMemberId: memberId,
+        localMyFavorite: true,
+      },
+      pipeline: [
+        {
+          $match: {
+            $expr: {
+              $and: [
+                { $eq: ["$likeRefId", "$$localLikeRefId"] },
+                { $eq: ["$memberId", "$$localMemberId"] },
+              ],
+            },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            memberId: 1,
+            likeRefId: 1,
+            myFavorite: "$$localMyFavorite",
+          },
+        },
+      ],
+      as: "meLiked",
+    },
+  };
 };
 
 export const lookupMember = {
-	$lookup: {
-		from: 'members',
-		localField: 'memberId',
-		foreignField: '_id',
-		as: "memberData",
-	}
-}
+  $lookup: {
+    from: "members",
+    localField: "memberId",
+    foreignField: "_id",
+    as: "memberData",
+  },
+};
 
 export const lookupFollowingData = {
-	$lookup: {
-		from: 'members',
-		localField: 'followingId',
-		foreignField: '_id',
-		as: 'followingData'
-	},
+  $lookup: {
+    from: "members",
+    localField: "followingId",
+    foreignField: "_id",
+    as: "followingData",
+  },
 };
 
 export const lookupFollowerData = {
-	$lookup: {
-		from: 'members',
-		localField: 'followerId',
-		foreignField: '_id',
-		as: 'followerData'
-	},
-}
+  $lookup: {
+    from: "members",
+    localField: "followerId",
+    foreignField: "_id",
+    as: "followerData",
+  },
+};
