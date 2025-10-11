8def8b1214e6054fe6fc03c2c5fb5a36da4982ee
Rakhimov Navruz <alina@Rakhimovs-MacBook-Pro.local>
2025-10-11 11:44:05 +0900

    fix: modify batch module and connect to MongoDB

--- apps/nestars-batch/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { NestarsBatchModule } from './nestars-batch.module';
-
-async function bootstrap() {
-	const app = await NestFactory.create(NestarsBatchModule);
-	await app.listen(process.env.SERVER_BATCH ?? 3000);
-}
-bootstrap();
--- apps/nestars-batch/src/nestars-batch.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class NestarsBatchService {
-  getHello(): string {
-    return 'Welcome to Batch Server!';
-  }
-}
